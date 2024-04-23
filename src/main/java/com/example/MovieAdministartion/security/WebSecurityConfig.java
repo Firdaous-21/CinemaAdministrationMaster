4d48a70e5ff485c5fe1d52dc610d8a38ca34f30e
Firdaous <158854039+Firdaous-21@users.noreply.github.com>
2024-04-23 13:03:23 +0100

    second comment

--- src/main/java/com/example/MovieAdministartion/security/WebSecurityConfig.java
+++ src/main/java/com/example/MovieAdministartion/security/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
